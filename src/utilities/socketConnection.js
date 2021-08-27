86744c70fdbf8171270770effd0884551c85ea3d
mauricioaznar <m_aznar@eogresources.com>
2021-08-27 08:04:13 -0500

    moremoremore

--- src/utilities/socketConnection.js
+++ src/utilities/socketConnection.js
@@ -3,8 +3,7 @@ import io from 'socket.io-client'
 
 const devUrl = 'http://localhost:4000'
 const serverUrl = 'https://performance-server.mauaznar.com'
-// const url = process.env.NODE_ENV === "development" ? devUrl : serverUrl
-const url = serverUrl
+const url = process.env.NODE_ENV === "development" ? devUrl : serverUrl
 
 let socket = io.connect(url)
 
